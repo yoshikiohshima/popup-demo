497c21c326139f785489fc79d78f27e12ca83a4f
Yoshiki Ohshima <yoshiki.ohshima@acm.org>
2023-06-22 08:59:13 -0700

    add apiKey.js

---
+++ apiKey.js
@@ -0,0 +1,11 @@
+const apiKey = "1hFnfucpqtkPJJeiGNDAAzOsJ4nbLJCP1Eps9O2EI";
+const appId = "io.croquet.yoshikiohshima.popup-demo";
+export default {apiKey, appId};
+
+// For development on local computer and local network, use a development key
+// from croquet.io/keys and create a file called apiKey-dev.js.
+
+// For deployment to a publicly accessible server, create a production key
+// from croquet.io/keys and create a file called apiKey.js.
+
+// you may export other Croquet session parameters to override default values.
